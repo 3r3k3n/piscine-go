afe65008b95811c87d156518f40cd29586db22dd
3r3k3n <3r3k3n@github.com>
2019-10-16 10:35:31 +0600

    1GO

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,13 +1,9 @@
 package main 
+import ("fmt")
 
-import ("github.com/01-edu/z01")
-
-
-func main()
-{
-	for i := 97: i <= 122; i++{
-		fmt.Printrune("i")
+func main(){
+	for i:=97; i<=122; i++{
+		fmt.Printf("%c",i)
 	}
-	fmt.println("\n")
-
+	fmt.Printf("\n")
 }
