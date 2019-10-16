0f41747ec2aae6cb88fcd47b4da1962a7f93e39e
3r3k3n <3r3k3n@github.com>
2019-10-16 14:51:52 +0600

    4GO

---
+++ isnegative/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func IsNegative(nb int) {
+	If nb>null{
+		z01.PrintRune('F')
+	}else{
+		z01.PrintRune('T')
+	}
+	z01.PrintRune(rune(10))
+}
