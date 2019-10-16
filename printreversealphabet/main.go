064cfe7b82f1075017c190b575bd95a80b82e746
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-16 12:57:26 +0600

    Update main.go

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -6,7 +6,9 @@ import (
 
 func main() {
 	for i := 'z'; i <= 'a'; i-- {
-		z01.PrintRune(rune(i))
+		z01.PrintRune(i)
+		if i == a {
+		z01.PrintRune(rune(10))
+		}
 	}
-	z01.PrintRune(rune(10))
 }
