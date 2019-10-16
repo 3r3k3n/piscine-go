b96c4279ed282e42e5120834c48515b660b7dc20
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-16 15:56:14 +0600

    Update isnegative.go

--- isnegative.go
+++ isnegative.go
@@ -4,12 +4,11 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func IsNegative(nb int) {
+func isnegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
 	} else {
 		z01.PrintRune('F')
 	}
 	z01.PrintRune('\n')
-}
 }
