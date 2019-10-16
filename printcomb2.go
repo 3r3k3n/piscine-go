da7378fcfd028241bd57b31858eca24026989976
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-16 19:51:52 +0600

    Update printcomb2.go

--- printcomb2.go
+++ printcomb2.go
@@ -9,7 +9,7 @@ func PrintComb2() {
 		for b := '0'; b <= '9'; b++ {
 			d := b + 1
 			for c := a; c <= '9'; c++ {
-				for ; d <= '9'; d++ {
+				for d = '0'; d <= '9'; d++ {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(' ')
@@ -21,7 +21,6 @@ func PrintComb2() {
 					z01.PrintRune(' ')
 				}
 			}
-			d = '0'
 		}
 	}
 	z01.PrintRune(10)
