f4c21d5ddc874515a5dda5fd92b5c6385a42d504
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-16 16:51:23 +0600

    Update isnegative.go

--- isnegative.go
+++ isnegative.go
@@ -10,11 +10,10 @@ func IsNegative(nb int) {
 	} else {
 		z01.PrintRune('F')
 	}
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
-
 func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
+	IsNegative(1)
+	IsNegative(0)
+	IsNegative(-1)
 }
