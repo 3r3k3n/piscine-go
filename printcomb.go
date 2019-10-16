a207e4d5b1fe4b384eccc39782c716c376a84bab
3r3k3n <3r3k3n@github.com>
2019-10-16 17:40:34 +0600

    5GO

--- printcomb.go
+++ printcomb.go
@@ -8,9 +8,9 @@ func PrintComb() {
 	for a := '0'; a <= '9'; a++ {
 		for b := a + 1; b <= '9'; b++ {
 			for c := b + 1; c <= '9'; c++ {
-				z01.PrintRune(rune(a))
-				z01.PrintRune(rune(b))
-				z01.PrintRune(rune(c))
+				z01.PrintRune(a)
+				z01.PrintRune(b)
+				z01.PrintRune(c)
 				if a != '7' || b != '8' || c != '9' {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
