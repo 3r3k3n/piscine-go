532ebe005a0727c5c01f168956b9630f669c8b74
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-17 20:58:11 +0600

    Update and rename basicatoi2.go to atoi.go

--- atoi.go
+++ atoi.go
@@ -1,6 +1,6 @@
 package piscine
 
-func intfor(r rune) int {
+func intfor3(r rune) int {
 	temp2 := -1
 	for temp := '0'; temp <= r; temp++ {
 		temp2++
@@ -8,20 +8,29 @@ func intfor(r rune) int {
 	return temp2
 }
 
-func BasicAtoi2(s string) int {
+func Atoi(s string) int {
+	bool1 := false
 	stroka := []rune(s)
 	n := 0
+	temp := 0
 	for range stroka {
 		n++
 	}
+	if n != 0 && stroka[0] == '-' {
+		bool1 = true
+		temp++
+	}
 	ans := 0
-	for i := 0; i < n; i++ {
+	for i := temp; i < n; i++ {
 		if stroka[i] < '0' || stroka[i] > '9' {
 			return 0
 		} else {
 			ans *= 10
-			ans += intfor(stroka[i])
+			ans += intfor3(stroka[i])
 		}
+	}
+	if bool1 == true {
+		ans = ans * -1
 	}
 	return ans
 }
