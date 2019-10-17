5fd0365d7977a4dcf168f075616ee2c5eafa6b6b
3r3k3n <3r3k3n@github.com>
2019-10-17 17:34:53 +0600

    str rev

---
+++ strrev.go
@@ -0,0 +1,20 @@
+package piscine
+
+
+
+func StrRev(s string) string {
+	var a int
+	
+	for index := range s {
+		a = index
+	}
+	
+	wordInBytes := []byte(s)
+	j := 0
+	for i:=a; i >= 0; i--{
+		wordInBytes[i] = byte(s[j])
+		j++
+	}
+	FinalStr := string(wordInBytes)  
+	return FinalStr
+}
