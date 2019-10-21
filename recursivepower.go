b663ecb327e65d06a9fa8d54a685ab7a4dd33d90
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-21 12:40:44 +0600

    Update recursivepower.go

--- recursivepower.go
+++ recursivepower.go
@@ -9,9 +9,9 @@ func RecursivePower(nb int, power int) int {
 		if power == 0 {
 			return 1
 		}
-		if nb =! 1 {
+		if nb > 1 || nb < -1 {
 			return RecursivePower(result, power-1) * result
-		}
+		} 
 
 	}
 	return result
