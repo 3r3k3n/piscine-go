408750010bdbeba57e7e5b3be944b4a68f02cd3f
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-21 12:30:26 +0600

    Update recursivefactorial.go

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -5,7 +5,7 @@ func RecursiveFactorial(nb int) int {
 	if nb == 1 {
 		return 1
 	}
-	if nb == 0 || nb > 50 || nb > 0 {
+	if nb == 0 || nb > 50 || nb <0 {
 		return 0
 	} else {
 		return nb * RecursiveFactorial(nb-1)
