e3619bdaf83e9223613bac124e0d9345bec9fabc
3r3k3n <3r3k3n@github.com>
2019-10-22 17:58:07 +0600

    Rune by n Letter

---
+++ nrune.go
@@ -0,0 +1,19 @@
+package piscine
+
+func NRune(s string, n int) rune {	
+	counterRune := []rune(s)
+	counter := 0
+	mainRune := 0
+	for range s {
+		counter++
+	} 
+		if n > counter {
+			return 0
+		} else if n <= 0 {
+			return '\x00'
+		}	
+	for i := 1; i < n; i++ {
+		mainRune = i
+	}
+	return counterRune[mainRune]
+}
