5e693987c6b38aef1b2e7fe425f28ae21972efaa
3r3k3n <3r3k3n@github.com>
2019-10-22 22:38:31 +0600

    fhkdjf

---
+++ isupper.go
@@ -0,0 +1,13 @@
+package piscine
+
+func IsUpper(str string) bool {
+	runes := []rune(str)
+	ok := true
+	for _, i := range runes {
+		if !(i >= 'A' && i <= 'Z') {
+			ok = false
+			return ok
+		}
+	}
+	return ok
+}
