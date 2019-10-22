a2ba8191cd53d810a8d664d460c60bc0a2157788
3r3k3n <3r3k3n@github.com>
2019-10-22 21:12:15 +0600

    upLetters

---
+++ touuper.go
@@ -0,0 +1,11 @@
+package piscine 
+
+func ToUpper(s string) string {
+	wordByte := []byte(s)
+	for index, letters := range wordByte {
+		if letters <= 122 && letters >= 97 {
+			wordByte[index] = letters - 32
+		}
+	}
+	return string(wordByte)
+}
