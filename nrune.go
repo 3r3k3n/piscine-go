4c6d2f87d6f773a53682c210264c66414c87c44d
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-22 18:02:41 +0600

    Update nrune.go

--- nrune.go
+++ nrune.go
@@ -6,12 +6,12 @@ func NRune(s string, n int) rune {
 	mainRune := 0
 	for range s {
 		counter++
-	} 
-		if n > counter {
-			return 0
-		} else if n <= 0 {
-			return '\x00'
-		}	
+	}
+	if n > counter {
+		return 0
+	} else if n <= 0 {
+		return '\x00'
+	}
 	for i := 1; i < n; i++ {
 		mainRune = i
 	}
