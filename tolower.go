79441d22676265c47a48b54978e2c1e9c8051570
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-22 21:30:40 +0600

    Update tolower.go

--- tolower.go
+++ tolower.go
@@ -3,7 +3,7 @@ package piscine
 func ToLower(s string) string {
 	wordByte := []byte(s)
 	for index, letters := range wordByte {
-		if letters <= 65 && letters >= 90 {
+		if letters <= 90 && letters >= 65 {
 			wordByte[index] = letters + 32
 		}
 	}
