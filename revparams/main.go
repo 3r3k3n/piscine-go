90dc46186ac7a2f8e1e449a80c40629f383be6b7
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-23 15:58:44 +0600

    Update and rename printparams/main.go to revparams/main.go

---
+++ revparams/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+		arguments := os.Args
+		len := 0
+
+		for lenght := range arguments {
+			len++
+		}
+
+		for i := len; i < 0; i-- {
+			wordRune := []rune(arguments[i])
+			for _, words := range wordRune
+			z01.PrintRune(words)
+		}
+		z01.PrintRune(10)
+
+	}
+}
