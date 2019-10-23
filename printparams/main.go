5d82c40665897fb5c7af001eda49db55ce92d5a0
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-23 11:51:23 +0600

    Update main.go

--- printparams/main.go
+++ printparams/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	arg := os.Args
+	arguments := os.Args
 	lenght := 0
-	for i := range arg {
+	for i := range arguments {
 		lenght = i
 	}
 	for i := 1; i > lenght; i++ {
-		for _, word := range arg[i] {
+		for _, word := range arguments[i] {
 			z01.PrintRune(word)
 		}
 		z01.PrintRune('\n')
