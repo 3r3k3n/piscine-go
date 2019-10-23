decac743351f23bd79850ec0c6eb41ed66ff8c6d
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-23 18:00:15 +0600

    Update main.go

--- sortparams/main.go
+++ sortparams/main.go
@@ -1,31 +1,36 @@
 package main
 
 import (
-	"github.com/01-edu/z01"
 	"os"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
-	args := os.Args
-	var runes []rune
-	res := ""
-	counter := 0
-	for i, v := range args {
-		if i != 0 {
-			counter++
-			res += v
-		}
+	arguments := os.Args
+	len := 0
+	for index := range arguments {
+		len = index + 1
 	}
-	runes = []rune(res)
-	for i := 0; i < counter-1; i++ {
-		for j := i + 1; j < counter; j++ {
-			if runes[i] > runes[j] {
-				unes[i], runes[j] = runes[j], runes[i]
+
+	table := []string(arguments[1:])
+	size := len - 1
+	var temp string
+	for i := 0; i < size-1; i++ {
+		for j := 0; j < (size - i - 1); j++ {
+			if table[j] > table[j+1] {
+				// меняем элементы местами
+				temp = table[j]
+				table[j] = table[j+1]
+				table[j+1] = temp
 			}
 		}
-		for _, v := range runes {
-			z01.PrintRune(v)
-			z01.PrintRune('\n')
+	}
+
+	for i := 0; i < len-1; i++ {
+		for _, letter := range table[i] {
+			z01.PrintRune(letter)
 		}
+		z01.PrintRune('\n')
 	}
 }
