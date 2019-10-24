fff4966ee420c77157f38773136fd7957d57c94a
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-24 14:24:13 +0600

    Update makerange.go

--- makerange.go
+++ makerange.go
@@ -4,14 +4,12 @@ func MakeRange(min, max int) []int {
 	if max <= min {
 		return nil
 	}
-	
 	lenght := (max - min)
 	answer := make([]int, lenght)
-	 
 	if min < max {
-		for i := 0; i < lenght ; i++ {
+		for i := 0; i < lenght; i++ {
 			answer[i] = i + min
 		}
-	} 
-	return answer 
+	}
+	return answer
 }
