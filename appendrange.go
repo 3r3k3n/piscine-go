ccbfc5eb87f9c5138383841c1f053cec37ad5e8d
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-24 13:57:43 +0600

    Update appendrange.go

--- appendrange.go
+++ appendrange.go
@@ -4,11 +4,11 @@ func AppendRange(min, max int) []int {
 	var answer []int
 
 	if min < max {
-		for i := min-1; i < max; i++ {
+		for i := min - 1; i < max; i++ {
 			answer = append(answer, i+1)
 		}
 		return answer
-		} else {
+	} else {
 		return nil
 	}
 }
