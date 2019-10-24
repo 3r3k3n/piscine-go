d1d22e80198562f4eaa2225c01b01e63456299bf
3r3k3n <3r3k3n@github.com>
2019-10-24 13:56:12 +0600

    append dinamic array

---
+++ appendrange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	var answer []int
+
+	if min < max {
+		for i := min-1; i < max; i++ {
+			answer = append(answer, i+1)
+		}
+		return answer
+		} else {
+		return nil
+	}
+}
