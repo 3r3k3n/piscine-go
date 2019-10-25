b8ca96316f67d79844eb045fefef2fa35addd2f0
Yerkhan <42962511+3r3k3n@users.noreply.github.com>
2019-10-25 19:00:20 +0600

    Update splitwhitespaces.go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,50 +1,15 @@
 package piscine
 
-func lent(d string) int {
-	inc := 0
-	for range d {
-		inc++
-	}
-	return inc
-}
+import (
+	"github.com/01-edu/z01"
+)
 
-func SplitWhiteSpaces(str string) []string {
-	TextToString := ""
-	startvalue := false
-	count_M := 0
-
-	for ii, v := range str {
-		if string(v) == " " || string(v) == "\t" || string(v) == "\n" {
-			if startvalue == true {
-				count_M++
-				startvalue = false
-			}
-		} else {
-			if startvalue == false {
-				startvalue = true
-			}
+func PrintWordsTables(table []string) {
+	for _, str := range table {
+		strAsRune := []rune(str)
+		for _, l := range strAsRune {
+			z01.PrintRune(l)
 		}
-		if ii == lent(str)-1 && startvalue == true {
-			count_M++
-
-		}
+		z01.PrintRune('\n')
 	}
-	t := make([]string, count_M)
-	k := 0
-	for i, v := range str {
-		if i == lent(str)-1 && string(v) != " " && string(v) != "\t" && string(v) != "\n" {
-			TextToString += string(v)
-			t[k] = TextToString
-		} else if string(v) != " " && string(v) != "\t" && string(v) != "\n" {
-			TextToString = TextToString + string(v)
-		} else {
-			if lent(TextToString) >= 1 {
-				t[k] = TextToString
-				k = k + 1
-			}
-			TextToString = ""
-		}
-
-	}
-	return t
 }
